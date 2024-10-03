37b3e74c023358090b83403c2896f594be26b17c
UzVPD <vladuz178@gmail.com>
2024-10-03 18:57:19 +0200

    modal window

--- public/dist/js/main.js
+++ public/dist/js/main.js
@@ -4,3 +4,25 @@ function softScroll(id) {
     }, 400) 
     return false;
 }
+
+$('.video-play, .close-btn').on('click', () => {
+    $('#modal-youtube').toggle();
+    $('body').toggleClass('overflow-hidden');
+    $('iframe').attr('src', 'https://www.youtube.com/embed/dQw4w9WgXcQ?si=RLhxiSksQdec-l-d;&amp;controls=0;&autoplay=1');
+    let video = $('iframe').attr('src');
+})
+
+$('.close-btn').on('click', () => {
+    $('iframe').attr('src', '');
+});
+
+$(window).on('resize', () => {
+    resizeVideo();
+}).resize();
+
+function resizeVideo() {
+    $('iframe').each(() => {
+        let width = $(this).width();
+        $(this).css('height', width / 1.77 + "px");
+    })
+}
