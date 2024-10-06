1c89639ebedfa46b4d1580aadcd117cbcdd1e154
UzVPD <vladuz178@gmail.com>
2024-10-06 19:04:34 +0200

    public -> docs

--- gulpfile.js
+++ gulpfile.js
@@ -6,35 +6,35 @@ const sass = require('gulp-sass')(require('sass'))
 const browserSync = require('browser-sync').create()
 
 gulp.task('buildCSS', async () => {
-    gulp.src('public/src/css/*.scss')
+    gulp.src('docs/src/css/*.scss')
         .pipe(sass.sync({ silenceDeprecations: ['legacy-js-api']}).on('error', sass.logError))
         .pipe(miniCSS())
         .pipe(rename({
             suffix: '.min'
         }))
-        .pipe(gulp.dest('public/dist/css'))
+        .pipe(gulp.dest('docs/dist/css'))
         .pipe(browserSync.stream())
 })
 
 gulp.task('buildJS', async () => {
-    gulp.src('public/src/js/*.js')
+    gulp.src('docs/src/js/*.js')
         .pipe(miniJS())
-        .pipe(gulp.dest('public/dist/js'))
+        .pipe(gulp.dest('docs/dist/js'))
         .pipe(browserSync.stream())
 })
 
 gulp.task('browser-sync', function() {
     browserSync.init({
         server: {
-            baseDir: "public/"
+            baseDir: "docs/"
         }
     });
-    gulp.watch("public/*.html").on('change', browserSync.reload);
+    gulp.watch("docs/*.html").on('change', browserSync.reload);
 });
 
 gulp.task('watchAll', () => {
-    gulp.watch("public/src/css/*.scss", gulp.series('buildCSS'))
-    gulp.watch("public/src/js/*.js", gulp.series('buildJS'))
+    gulp.watch("docs/src/css/*.scss", gulp.series('buildCSS'))
+    gulp.watch("docs/src/js/*.js", gulp.series('buildJS'))
 })
 
 gulp.task('default', gulp.parallel('browser-sync', 'watchAll'))
